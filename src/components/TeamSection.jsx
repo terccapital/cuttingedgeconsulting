8c36a78f726bb8fcd5062008668bef7c689e4aad
Douglas Terc <douglasterc@douglass-mbp-2.mynetworksettings.com>
2025-02-19 12:16:41 -0500

    again trying to fix headshots

--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -8,7 +8,7 @@ const TeamSection = () => {
 
       <div className="team-member">
         <div className="team-image">
-          <img src="../public/doug-terc.png" alt="Douglas Terc" />
+          <img src="/doug-terc.png" alt="Douglas Terc" />
         </div>
         <div className="team-info">
           <h3 className="team-name">Douglas Terc</h3>
@@ -24,7 +24,7 @@ const TeamSection = () => {
 
       <div className="team-member">
         <div className="team-image">
-          <img src="../public/timin-sonik.png" alt="Timin Sonik" />
+          <img src="/timin-sonik.png" alt="Timin Sonik" />
         </div>
         <div className="team-info">
           <h3 className="team-name">Timin Sonik</h3>
